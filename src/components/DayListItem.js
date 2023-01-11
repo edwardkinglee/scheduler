9df035947c32a2e726e579592f5fab3f4ff5113f
edwardkinglee <edward.lee3@telus.com>
2023-01-11 17:14:14 -0500

    Update InterviewList clickable story

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -29,7 +29,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,7 +9,6 @@ export default function InterviewerList(props) {
     return (
      <InterviewerListItem 
       key={interviewer.id}
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.interviewer}
