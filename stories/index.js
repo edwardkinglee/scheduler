68134d4ec1fa270c701b757140cb7911ddd5dc89
edwardkinglee <edward.lee3@telus.com>
2023-01-11 23:27:13 -0500

    Add Empty and Header components

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,10 +136,12 @@ storiesOf("Button", module)
     />
   ));
 
-    storiesOf("Appointment", module)
+  
+       storiesOf("Appointment", module)
   .addParameters({
-      backgrounds: [{ name: "white", value: "#fff", default: true }]
-    })
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => (
-       <Appointment time="12pm" />));
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
