1746a04696c6380ae741c69f502535375f2a0541
edwardkinglee <edward.lee3@telus.com>
2023-01-17 20:29:23 -0500

    Add useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  const transition = (newMode, replace = false) => {
+   
+    if(replace){
+      setHistory((prev) => [...prev.slice(0, prev.length -1), newMode]);
+    }
+
+    if(!replace){
+      setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() { 
+    if(history.length > 1){
+      setHistory((prev) => [...prev.slice(0, prev.length -1)]);
+    }
+    
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
